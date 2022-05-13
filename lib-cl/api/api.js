401a74d7f19fe54db57e191e7a6fc0dc68356da8
Jordan Oyenusi <jordanoyenusi@googlemail.com>
2022-05-13 20:39:44 +0100

    Initial Commit

---
+++ lib-cl/api/api.js
@@ -0,0 +1,6 @@
+import useSWR from "swr";
+import fetch from 'unfetch';
+
+const OPENAQ_API_BASE_URL = process.env.NEXT_PUBLIC_OPENAQ_API_BASE_URL;
+const fetcher = url => fetch(`${OPENAQ_API_BASE_URL}/${url}`).then(r => r.json());
+export const useAllCitiesGB = () => useSWR('/cities?limit=1000&page=1&offset=0&sort=asc&country_id=GB&order_by=city', fetcher);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      color: {
+        purple: '#7836af',
+        sky: '#4376af'
+      }
+    },
+  },
+  plugins: [],
+}
