822ee830cf0b4af752f8beeab96bc41eb2ad2f62
Jordan Oyenusi <jordanoyenusi@googlemail.com>
2022-05-16 14:18:23 +0100

    fix vercel

--- lib-cl/api/api.js
+++ lib-cl/api/api.js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import fetch from 'unfetch';
 
-const OPENAQ_API_BASE_URL = process.env.NEXT_PUBLIC_OPENAQ_API_BASE_URL;
+const OPENAQ_API_BASE_URL = 'https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2';
 const fetcher = url => fetch(`${OPENAQ_API_BASE_URL}/${url}`).then(r => r.json());
 
 export const useAllCitiesGB = () => useSWR('/cities?limit=1000&page=1&offset=0&sort=asc&country_id=GB&order_by=city', fetcher);
